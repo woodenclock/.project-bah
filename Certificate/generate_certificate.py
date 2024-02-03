e9bf7a12dad27aa5192e96e2e4cbd2c2c9efd028
andrefoo <76435197+andrefoo@users.noreply.github.com>
2024-02-03 12:08:03 +0800

    Update generate_certificate.py
    Update font sizing and alignment
    Add parameters for date of completion and course completed

--- Certificate/generate_certificate.py
+++ Certificate/generate_certificate.py
@@ -2,35 +2,44 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def create_certificate(
-    name, template_path, font_path, font_size, position, output_path
+    name, date, course
 ):
+    template_path = "Certificate/certificate_template.png"
+    font_path = "Certificate/times.ttf"
+    name_position = (1000, 707)
+    output_path = "Certificate/personalized_certificate.png"
+
     # Load the certificate template
     image = Image.open(template_path)
     draw = ImageDraw.Draw(image)
 
     # Load the font
-    font = ImageFont.truetype("Certificate/times.ttf", 40)
+    name_font = ImageFont.truetype(font_path, 150)
+    # Add name text
+    name_text_width = draw.textlength(name, font=name_font)
+        # Calculate the position
+    x = name_position[0] - name_text_width / 2
+    y = name_position[1] - 80
+    draw.text((x, y), name, font=name_font, align="center", fill="black")
 
-    # Calculate text width and height
-    text_width, text_height = 200, 200
 
-    # Calculate the position: this example assumes 'position' is the center where the name should be placed
-    x = position[0] - text_width / 2
-    y = position[1] - text_height / 2
+    # Add date text
+    date_font = ImageFont.truetype(font_path, 40)
+    date_text_width = draw.textlength(date, font=date_font)
+    draw.text((550 - date_text_width / 2, 1010), date, font=date_font, align="center", fill="black")
 
-    # Position adjustment might be necessary depending on your template
-    draw.text((x, y), name, font=font, align="center", fill="black")
+    # Add course name text
+    course_font = ImageFont.truetype(font_path, 30)
+    course_text_width = draw.textlength(course, font=course_font)
+    draw.text((1000 - course_text_width / 2, 915), course, font=course_font, align="center", fill="black")
 
     # Save the modified image
     image.save(output_path)
 
-
 # Example usage
-name = "John Doe"
-template_path = "Certificate/certificate_template.png"
-font_path = "Certificate/times.ttf"  # Example font path
-font_size = 40
-position = (500, 300)  # This should be adjusted based on your template
-output_path = "Certificate/personalized_certificate.png"
+name = "Katelyn Choo"
+date = "21 September 2023"
+course = "Digital Literacy Trainer 1"
 
-create_certificate(name, template_path, font_path, font_size, position, output_path)
+
+create_certificate(name, date, course)
