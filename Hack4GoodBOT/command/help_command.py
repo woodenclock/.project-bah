1599dc8f156543f316f4f76dd1935ed82a4931c8
Lee Sungmin <luckyisland3710@gmail.com>
2024-02-04 23:04:44 +0800

    Update Hack4GoodBOT

--- Hack4GoodBOT/command/help_command.py
+++ Hack4GoodBOT/command/help_command.py
@@ -8,10 +8,10 @@ from telegram.ext import (Application, CommandHandler, ConversationHandler, Mess
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         "/enroll - Enroll as a new volunteer\n"
-        "/browse - Browse volunteering opportunities\n"
-        "/register - Check opportunities I have signed up\n"
-        "/attended - Check attended opportunities\n"
-        "/upcoming - Check upcoming opportunities\n"
+        "/browse - Browse volunteering events\n"
+        "/register - Check events  I have signed up\n"
+        "/attended - Check attended events\n"
+        "/upcoming - Check upcoming events\n"
         "/feedback - Submit feedback/blog/reflection\n"
         "/certificate - Request a certificate for attended events"
     )
